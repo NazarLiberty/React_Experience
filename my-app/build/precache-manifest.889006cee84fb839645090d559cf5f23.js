d286181581589523d56b923cf4f1fb9773694400
NazarLiberty <NazarLiberty@gmail.com>
2020-05-11 18:23:47 +0300

    gitignore

---
+++ my-app/build/precache-manifest.889006cee84fb839645090d559cf5f23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486392561e589dcfdd6a1144fad9ca9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddf0c75b25af09b3c042",
+    "url": "/static/css/main.d7f66f82.chunk.css"
+  },
+  {
+    "revision": "05afa3b6ab6ef90792bf",
+    "url": "/static/js/2.43934bdf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.43934bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf0c75b25af09b3c042",
+    "url": "/static/js/main.cde9a246.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
